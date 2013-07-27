5256c09424d085e2ae4b0c08b000ae115d6ff949
Sathya Prakash <sathyaprakash222@gmail.com>
2013-07-27 09:28:41 +0530

    Add Redirect after Insert

--- deletetodo.php
+++ deletetodo.php
@@ -1,27 +1,24 @@
 <?php
 include("dbc.php");
 mysql_select_db('wrdp');
-
-
- $checkbox = $_POST['Checkbox'];
-
- $del_id  = $checkbox[0];
-
-if ($_POST[deleteo] == delete) {
-
-$sql = "DELETE FROM intel where id = $del_id";
+$checkbox = $_POST['Checkbox'];
+$del_id  = $checkbox[0];
+if ($_POST[deleteo] == delete) 
+{
+    $sql = "DELETE FROM intel where id = $del_id";
 }
-else {
- $sql = "DELETE FROM todo where id = $del_id";
-  }
- $result = mysql_query($sql) or die(mysql_error());
-
-			 if($result)
-		 {	      echo "Sucess !!";
-				 header('Location: index.php');
-			 }
-			 else
-			 {
- echo "Error: ".mysql_error();
-			 }
- ?>
+else
+{
+    $sql = "DELETE FROM todo where id = $del_id";
+}
+$result = mysql_query($sql) or die(mysql_error());
+if($result)
+{
+    echo "Sucess !!";
+    header('Location: index.php');
+}
+else
+{
+    echo "Error: ".mysql_error();
+}
+?>
---
+++ insert.php
@@ -0,0 +1,31 @@
+<?php
+include("dbc.php");
+mysql_select_db('wrdp');
+if ($_POST[intelo] == intel) 
+{
+    $query = sprintf("INSERT INTO intel (date, name, content) VALUES (CURDATE(),'%s','%s')", mysql_real_escape_string($_POST[input01]),mysql_real_escape_string($_POST[textarea]));
+ }
+else
+{
+
+    $query = sprintf("INSERT INTO todo (todo) VALUES ('%s')",
+    mysql_real_escape_string($_POST[textarea]));
+}
+$result = mysql_query($query);
+
+if (!$result) 
+{
+    $message  = 'Invalid query: ' . mysql_error() . "\n";
+    $message .= 'Whole query: ' . $query;
+    die($message);
+} 
+else 
+{ 
+    echo  "Posted Successfully !!";
+    header('Location: index.php');
+}
+
+
+?>
+
+
