79a4058e4b5b81da3e3cac3b53725aee19ba056f
Sathya Prakash <sathyaprakash222@gmail.com>
2013-07-28 15:48:16 +0530

    Deploy script update --missed redditparse

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-cp baseframe-packed.css dashboard/dashboard.php  index.php input.php insert.php delete.php deletetodo.php bootstrap.css /var/www/intel/
+cp baseframe-packed.css dashboard/dashboard.php redditparse.php index.php input.php insert.php delete.php deletetodo.php bootstrap.css /var/www/intel/
 cp -R reddit-api-client /var/www/intel/
 if [ $? -eq 0 ]
 then echo "Deployed Succesfully";
