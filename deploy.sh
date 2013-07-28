c8882fb34d283cc5fa3f3f87bc7bcda42dc470f0
Sathya Prakash <sathyaprakash222@gmail.com>
2013-07-28 15:28:02 +0530

    Change deploy script to load submodules

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-cp baseframe-packed.css dashboard/dashboard.php index.php input.php insert.php delete.php deletetodo.php bootstrap.css /var/www/intel/
+cp baseframe-packed.css dashboard/dashboard.php  index.php input.php insert.php delete.php deletetodo.php bootstrap.css /var/www/intel/
+cp -R reddit-api-client /var/www/intel/
 if [ $? -eq 0 ]
 then echo "Deployed Succesfully";
 fi
