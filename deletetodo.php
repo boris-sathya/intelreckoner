8d5d7a464bbb4a887b249fbd649583444969d90c
Sathya Prakash <sathyaprakash222@gmail.com>
2013-07-28 14:28:59 +0530

    #5 FIX Delete Multiple Intel items

--- deletetodo.php
+++ deletetodo.php
@@ -2,16 +2,11 @@
 include("dbc.php");
 mysql_select_db('wrdp');
 $checkbox = $_POST['Checkbox'];
-$del_id  = $checkbox[0];
-if ($_POST[deleteo] == delete) 
-{
-    $sql = "DELETE FROM intel where id = $del_id";
+foreach ($checkbox as $item)
+{ 
+ $sql = "DELETE FROM intel where id = $item";
+ $result = mysql_query($sql) or die(mysql_error());
 }
-else
-{
-    $sql = "DELETE FROM todo where id = $del_id";
-}
-$result = mysql_query($sql) or die(mysql_error());
 if($result)
 {
     echo "Sucess !!";
