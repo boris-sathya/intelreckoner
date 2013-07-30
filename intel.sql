647fe44ae1343e5ca1dfc95785f38a873969c99c
Sathya Prakash <sathyaprakash222@gmail.com>
2013-07-30 20:49:17 +0530

    Cleanup

--- intel.sql
+++ intel.sql
@@ -1,19 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.3.2deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jul 29, 2013 at 08:16 PM
--- Server version: 5.1.41
--- PHP Version: 5.4.17-1~lucid+1
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `wrdp`
